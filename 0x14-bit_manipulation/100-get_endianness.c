bca3b77227bf49d49849596ce9808785bbb50a59
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-04-10 18:16:00 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	v_int32_t fff = 0x12345678;
+	v_int8_t *ggg = (v_int8_t *)&fff;
+
+	if (*ggg == 0x78)
+		return (1);
+	else
+		return (0);
+}
