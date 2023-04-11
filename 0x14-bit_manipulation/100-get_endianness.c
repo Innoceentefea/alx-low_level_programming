7fdbf3a26fae2c6dcf63dd7de7896fa7b498613f
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-04-10 18:24:30 -0700

    get_endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,22 +4,15 @@
  * get_endianness - function that checks the endianness
  *
  * Return: 0 if big endian, 1 if little endian
- */
+*/
 
-#include "main.h"
-
-/**
- * get_endianness - function that checks the endianness
- *
- * Return: 0 if big endian, 1 if little endian
- */
 int get_endianness(void)
 {
-	unsigned int h;
-	char *c;
+	unsigned int b = 1;
+	char *a = (char *)&b;
 
-	h = 1;
-	s = (char *) &h;
-
-	return ((int)*s);
+	if (*a)
+		return (1);
+	else
+		return (0);
 }
