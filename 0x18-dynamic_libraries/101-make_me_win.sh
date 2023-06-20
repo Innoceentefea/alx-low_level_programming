82a6905c4d8e8c7af2b6fe92d42a0639b10128c3
INNOXSE <innocentefeakpoyibo@gmail.com>
2023-06-20 08:09:52 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-wget -O /tmp/libtest.so https://github.com/INNOXSE/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so 
+wget -P /tmp/libtest.so https://github.com/INNOXSE/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so 
  
 export LD_PRELOAD=/tmp/libtest.so 
